201e0e01f23c5068fdf16cfe5f8ae42186ce02ae
Muiz Khan <muiz@replenium.com>
2019-07-04 18:44:05 +0500

    Navbar incomplete

--- src/App.js
+++ src/App.js
@@ -4,12 +4,10 @@ import "./App.css";
 class App extends Component {
   render() {
     const name = "John Doe";
-    const showName = true;
-    const loading = false;
+
     return (
       <div className="App">
         <h1>Hello from the other side, you will enjoy learning it.</h1>
-        {loading ? <h4>Loading...</h4> : <h1>Yo {showName ? name : null}</h1>}
       </div>
       // <Fragment>
       //   <h1>Hello, i'm learning react</h1>

9cfa3f76d569801d4b8433b89171d0cca5597a9c
Muiz Junaid Khan <mzkhan25@hotmail.com>
2019-07-05 23:09:24 +0500

    Basic props and font awesome implemented

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Navbar />
-        <h1>Hello from the other side, you will enjoy learning it.</h1>
+        <Navbar icon="fab fa-github" />
       </div>
       // <Fragment>
       //   <h1>Hello, i'm learning react</h1>

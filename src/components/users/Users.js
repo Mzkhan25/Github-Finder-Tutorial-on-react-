bfea1667afb2bcbe488128b035ae0a0097cd9f9c
Muiz Khan <muiz@replenium.com>
2019-07-09 20:20:19 +0500

    Loader implemented

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/spinner/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: "grid",
@@ -19,4 +23,8 @@ const userStyle = {
   gridGap: "1rem"
 };
 
+Users.prototype = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 export default Users;

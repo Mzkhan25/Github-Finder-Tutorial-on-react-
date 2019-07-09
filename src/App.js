2d8781dff673272c70b976774214e97ab7e42e4c
Muiz Khan <muiz@replenium.com>
2019-07-09 20:06:58 +0500

    Https calls and basic states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
 import Users from "./components/users/Users";
+import axios from "axios";
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const response = await axios.get("https://api.github.com/users");
+    this.setState({ users: response.data, loading: false });
+  }
   render() {
     return (
       <div className="App">
         <Navbar icon="fab fa-github" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
